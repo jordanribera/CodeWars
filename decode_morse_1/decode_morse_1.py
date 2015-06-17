48662b200ed39daf8e8cae9551fef4718d5dfbc3
Jordan Ribera <jordanribera@gmail.com>
2015-06-17 10:02:33 -0700

    decode morse code, part 1

---
+++ decode_morse_1/decode_morse_1.py
@@ -0,0 +1,18 @@
+def decodeMorse(morseCode):
+    print morseCode
+    morseCode = morseCode.strip()
+    words = morseCode.split("   ")
+    outputWords = []
+    output = " "
+    
+    for thisWord in words:
+        thisOutputWord = ""
+        letters = thisWord.split(" ")
+        for thisLetter in letters:
+            if thisLetter in MORSE_CODE:
+                thisOutputWord += MORSE_CODE[thisLetter]
+        outputWords.append(thisOutputWord)
+        
+    output = output.join(outputWords)
+        
+    return output
