5d03b8fbf9d78574b16426749f40e8eb60576285
Jordan Ribera <jordanribera@gmail.com>
2015-06-25 13:57:25 -0700

    prefill array (list)

---
+++ prefill_array/prefill_array.py
@@ -0,0 +1,25 @@
+def prefill(n,v="undefined"):
+    if (n is None) or (representsInt(n) == False):
+        raise TypeError(str(n) + ' is invalid')
+
+    if int(n) == 0:
+        return []
+    
+    output = list()
+    n_int = 0
+    try:
+        n_int = int(n)
+    except:
+        n_int = 0
+        
+    for x in range (0, n_int):
+        output.append(v)
+        
+    return output
+    
+def representsInt(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
