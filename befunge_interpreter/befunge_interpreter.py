cf443785028d653b8cc6a6b1504bb3533d8bf1a4
Jordan Ribera <jordanribera@gmail.com>
2015-06-25 18:09:12 -0700

    badass

---
+++ befunge_interpreter/befunge_interpreter.py
@@ -0,0 +1,154 @@
+def interpret(code):
+    output = ""
+    direction = "left"
+    directions = ["up", "down", "left", "right"]
+    code = code.split("\n")
+    for x in range(0, len(code)):
+        code[x] = list(code[x])
+    #print code
+    running = True
+    safeCounter = 0
+    pointerX = 0
+    pointerY = 0
+    stack = []
+    while (running):
+        #read pointer location
+        value = code[pointerY][pointerX]
+        
+        #simple flow
+        if (value == "^"):
+            direction = "up"
+        if (value == "v"):
+            direction = "down"
+        if (value == "<"):
+            direction = "left"
+        if (value == ">"):
+            direction = "right"
+            
+        #advanced flow
+        if (value == "?"):
+            direction = directions[randint(0,3)]
+        if (value == "_"):
+            a = stack.pop()
+            if (a == 0):
+                direction = "right"
+            else:
+                direction = "left"
+        if (value == "|"):
+            a = stack.pop()
+            if (a == 0):
+                direction = "down"
+            else:
+                direction = "up"
+        if (value == "@"):
+            running = False
+                
+        #misc stack manipulation
+        if (value == ":"):
+            if (len(stack) == 0):
+                stack.append(0)
+            else:
+                a = stack.pop()
+                stack.append(a)
+                stack.append(a)
+        if (value == "\\"):
+            if (len(stack) > 2):
+                stack.append(0)
+            else:
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(a)
+                stack.append(b)
+        if (value == "$"):
+            a = stack.pop()
+        
+        #output
+        if (value == "."):
+            output = output + str(stack.pop())
+            print "output: " + output
+            
+        
+            
+        #numers onto stack
+        if representsInt(value):
+            stack.append(int(value))
+            print stack
+            
+        #addition
+        if (value == "+"):
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(a+b)
+            
+        #subtraction
+        if (value == "-"):
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(a-b)
+        
+        #multiplication
+        if (value == "*"):
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(a*b)
+            
+        #integer division
+        if (value == "/"):
+            a = stack.pop()
+            b = stack.pop()
+            if (a == 0):
+                stack.append(0)
+            else:
+                stack.append(b//a)
+                
+        #modulus
+        if (value == "%"):
+            a = stack.pop()
+            b = stack.pop()
+            if (a == 0):
+                stack.append(0)
+            else:
+                stack.append(b%a)
+                
+        #logical not
+        if (value == "!"):
+            a = stack.pop()
+            if (a == 0):
+                stack.append(1)
+            else:
+                stack.append(0)
+                
+        #greater than
+        if (value == "`"):
+            a = stack.pop()
+            b = stack.pop()
+            if (b > a):
+                stack.append(1)
+            else:
+                stack.append(0)
+                
+        
+        
+            
+        print value
+        
+        #move the pointer
+        if (direction == "up"):
+            pointerY = (pointerY - 1) % len(code)
+        if (direction == "down"):
+            pointerY = (pointerY + 1) % len(code)
+        if (direction == "left"):
+            pointerX = (pointerX - 1) % len(code[0])
+        if (direction == "right"):
+            pointerX = (pointerX + 1) % len(code[0])
+        safeCounter = safeCounter + 1
+        if (safeCounter > 100):
+            running = False
+    return output
+    
+def representsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
