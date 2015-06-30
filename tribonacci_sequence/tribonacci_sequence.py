86e6c6b604b4ac5f99ed2d6e3309e4dc765d8aa0
Jordan Ribera <jordanribera@gmail.com>
2015-06-29 18:33:36 -0700

    trib

---
+++ tribonacci_sequence/tribonacci_sequence.py
@@ -0,0 +1,9 @@
+def tribonacci(signature,n):
+    seq = signature
+    if sum(signature) > 100 or n == 0:
+        return []
+    if n < len(signature):
+        return signature[-n:]
+    for i in range(0,n - len(signature)):
+        seq.append(seq[-1] + seq[-2] + seq[-3])
+    return seq[-n:]
