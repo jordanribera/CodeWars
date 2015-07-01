5b71e0ed3ccae13c77c94b5b23fb1340d04a4354
Jordan Ribera <jordanribera@gmail.com>
2015-07-01 13:31:46 -0700

    xbonacci

---
+++ xbonacci/xbonnaci.py
@@ -0,0 +1,8 @@
+def Xbonacci(signature,n):
+    bracket = len(signature)
+    for x in range(0, n - bracket):
+        newValue = 0
+        for i in range(1, bracket + 1):
+            newValue += signature[-i]
+        signature.append(newValue)
+    return signature[:n]
