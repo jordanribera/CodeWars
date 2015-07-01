40dffb9e7d4b0cc9c882de251bf5cb963f9b3af2
Jordan Ribera <jordanribera@gmail.com>
2015-06-30 18:42:49 -0700

    got it working

--- get_the_loop/get_the_loop.py
+++ get_the_loop/get_the_loop.py
@@ -1,16 +1,18 @@
 def loop_size(node):
-    pastNodes = []
-    loopcount = 0
-    searching = True
-    while searching:
-        if node.next in pastNodes:
-            searching = False
-        else:
-            pastNodes.append(node.next)
-            node = node.next
-        loopcount += 1
-        if loopcount > 10000:
-            searching = False
-    print loopcount
-    print pastNodes
-    return (len(pastNodes)) - (pastNodes.index(node.next))
+    tortNode = node
+    hareNode = node.next.next
+    while True:
+        tortNode = tortNode.next
+        hareNode = hareNode.next.next
+        if (hareNode.next == tortNode) or (hareNode == tortNode):
+            break
+
+    lapNode = tortNode.next
+    searchNode = lapNode.next
+    loopSize = 1
+    while True:
+        if searchNode == lapNode:
+            break
+        searchNode = searchNode.next
+        loopSize += 1
+    return loopSize
