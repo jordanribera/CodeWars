c2effc051da82462cc0202cd041753ed429abfab
Jordan Ribera <jordanribera@gmail.com>
2015-07-02 13:03:07 -0700

    still not thin enough

--- extreme_fib/extreme_fib.py
+++ extreme_fib/extreme_fib.py
@@ -1,7 +1,5 @@
-def fib(n, computed={0:0, 1:1}):
-    if n not in computed:
-        if n < 0:
-            computed[n] = 0 - (fib(n+1, computed) - fib(n+2, computed))
-        else:
-            computed[n] = fib(n-1, computed) + fib(n-2, computed)
-    return computed[n]
+def f(n):
+    a, b = 0, 1
+    for i in range(0, n):
+        a, b = b, a + b
+    return a
