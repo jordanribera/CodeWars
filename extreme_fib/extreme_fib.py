7313ced429da4cee4154fe5c4421f983f5989338
Jordan Ribera <jordanribera@gmail.com>
2015-07-02 13:04:13 -0700

    straight iterative still isn't fast enough

--- extreme_fib/extreme_fib.py
+++ extreme_fib/extreme_fib.py
@@ -1,5 +1,11 @@
-def f(n):
+def fib(n):
+    print "n: " + str(n)
+    abs_n = abs(n)
     a, b = 0, 1
-    for i in range(0, n):
-        a, b = b, a + b
+    for i in range(0, abs_n):
+        if n < 0:
+            a, b = b, a - b
+        else:
+            a, b = b, a + b
+    #print "a: " + str(a)
     return a
