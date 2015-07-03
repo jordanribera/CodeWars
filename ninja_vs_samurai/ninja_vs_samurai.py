ab7ba5b315df8b7c5fd1dbd4d0d33b64e008019e
jordanribera <jordanribera@gmail.com>
2015-07-02 20:18:55 -0700

    vasoconstrictor

---
+++ ninja_vs_samurai/ninja_vs_samurai.py
@@ -0,0 +1,27 @@
+Position = {'high': 'h','low': 'l'}
+
+class Warrior():
+    def __init__(self, name):
+        #each warrior should be created with a name and 100 health points
+        self.name=name
+        self.health=100
+        #default guard is "", that is unguarded
+        self.block=""
+        self.deceased=False
+        self.zombie=False
+        
+    def attack(self,enemy,position):
+        #attacking high deals 10 damage, low 5
+        #0 damage if the enemy blocks in the same position
+        damage = 0
+        if enemy.block!=position: damage+=10 if position==Position['high'] else 5
+        #and even more damage if the enemy is not blocking at all
+        if enemy.block=="": damage+=5
+        enemy.set_health(enemy.health-damage)
+    
+    def set_health(self,new_health):
+        #health cannot have negative values
+        self.health=max(0,new_health)
+        #if a warrior is set to 0 health he is dead
+        if self.health==0:
+            self.deceased=True
